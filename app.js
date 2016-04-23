7c5a5250b5d0dbe4bbfd89c6cac211e0e01fa4e2
Himanshu <himanshu89.nsit@gmail.com>
2016-04-23 21:28:29 +0530

    test commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+
+app = express();
+
+//configure app
+
+
+
+//define routes
+
+app.get('/', function(req, res) {
+   res.json({login: true});  
+});
+
+
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function(){
+   console.log('server is running on port '+port); 
+}); 
+
+
+
